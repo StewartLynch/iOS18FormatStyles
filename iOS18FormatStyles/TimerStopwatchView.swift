f40acb2311bbf18e9f86615ca69d1b54b1ade79a
Stewart Lynch <slynch@createchsol.com>
2024-07-25 18:53:40 -0700

    Updated File Header

--- iOS18FormatStyles/TimerStopwatchView.swift
+++ iOS18FormatStyles/TimerStopwatchView.swift
@@ -1,9 +1,16 @@
 //
-//  TimerStopwatchView.swift
-//  iOS18FormatStyles
+// ----------------------------------------------
+// Original project: iOS18FormatStyles
+// by  Stewart Lynch on 2024-07-24
 //
-//  Created by Stewart Lynch on 2024-07-24.
-//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+// ----------------------------------------------
+// Copyright © 2024 CreaTECH Solutions. All rights reserved.
 
 import SwiftUI
 
